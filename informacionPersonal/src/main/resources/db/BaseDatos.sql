4366404a8a5e796952248395e392905cde509077
Jeyson Giovanni Correa Martinez <jcorrea@educacionbogota.gov.co>
2024-06-18 09:00:51 -0500

    incorporacio de schema se suprimen puertos en microservicios , se incorpora validacion de estadode salud en postgres

--- informacionPersonal/src/main/resources/db/BaseDatos.sql
+++ informacionPersonal/src/main/resources/db/BaseDatos.sql
@@ -1,3 +1,55 @@
+--Schema de las tablas utilizadas
+
+CREATE TABLE IF NOT EXISTS personas (
+  persona_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  nombre VARCHAR(255) NOT NULL,
+  genero VARCHAR(255) NOT NULL,
+  edad INT NOT NULL,
+  identificacion VARCHAR(255) NOT NULL,
+  direccion VARCHAR(255) NOT NULL,
+  telefono VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+  cliente_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  contrasenia VARCHAR(255),
+  estado BOOLEAN,
+  persona_id BIGINT NOT NULL,
+  FOREIGN KEY (persona_id) REFERENCES personas(persona_id)
+);
+
+CREATE TABLE IF NOT EXISTS entidades (
+  entidad_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  nombre VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS cuentas (
+  cuenta_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  numero_cuenta VARCHAR(255),
+  tipo VARCHAR(255),
+  saldo_inicial FLOAT,
+  estado BOOLEAN,
+  cliente_id BIGINT,
+  entidad_id BIGINT,
+  FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id),
+  FOREIGN KEY (entidad_id) REFERENCES entidades(entidad_id)
+);
+
+CREATE TABLE IF NOT EXISTS movimientos (
+  movimiento_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  fecha DATE,
+  tipo_movimiento VARCHAR(255),
+  valor FLOAT,
+  saldo FLOAT,
+  entidad_id BIGINT,
+  cuenta_id BIGINT,
+  FOREIGN KEY (entidad_id) REFERENCES entidades(entidad_id),
+  FOREIGN KEY (cuenta_id) REFERENCES cuentas(cuenta_id)
+);
+
+
+
+
 -- Inserción de datos en PERSONAS
 INSERT INTO PERSONAS ( NOMBRE, GENERO, EDAD, IDENTIFICACION, DIRECCION, TELEFONO)
 VALUES
@@ -11,11 +63,11 @@ VALUES
 -- Inserción de datos en CLIENTES (relacionando con PERSONAS)
 INSERT INTO CLIENTES ( CONTRASENIA, ESTADO, PERSONA_ID)
 VALUES
-    ( 'contraseña123', true, SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='123456789'),
-    ( 'password456', true, SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='987654321'),
-    ( 'secure789', false, SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='555555555'),
-    ( 'clave101', true, SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='111111111'),  -- Un cliente adicional para la persona 1
-    ( 'pass202', false, SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='222222222');
+    ( 'contraseña123', true, (SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='123456789')),
+    ( 'password456', true, (SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='987654321')),
+    ( 'secure789', false, (SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='555555555')),
+    ( 'clave101', true, (SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='111111111')),  -- Un cliente adicional para la persona 1
+    ( 'pass202', false, (SELECT PERSONA_ID FROM PERSONAS WHERE IDENTIFICACION='222222222'));
 
 
  -- Inserts para la tabla ENTIDADES en PostgreSQL
